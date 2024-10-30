7e62cabc670c58355fca268bba7b252c7fce3505
johnnyltk999 <johnnyltk999@gmail.com>
2024-10-30 15:09:10 +0700

    fix

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -5,30 +5,30 @@ import Vision from "../../../../public/pictures/vision.png";
 export default function Page() {
   return (
     <section id="about">
-      <div className="container mx-auto p-10 bg-background text-default rounded-xl">
+      <div className="container p-10 mx-auto bg-background text-default rounded-xl">
         {/* Title */}
-        <h1 className="text-primaryBase text-4xl font-extrabold text-center my-16">
+        <h1 className="my-16 text-4xl font-extrabold text-center text-primaryBase">
           ກ່ຽວກັບເຮົາ
         </h1>
 
         {/* Two Columns Layout */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+        <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
           {/* Left Column - Image and Vision */}
-          <div className="flex flex-col items-center bg-card border border-gray-500 rounded-lg p-6 shadow-md">
-            <h2 className="text-primaryBase text-2xl font-bold mb-6">
+          <div className="flex flex-col items-center p-6 border border-gray-500 rounded-lg shadow-md bg-card">
+            <h2 className="mb-6 text-2xl font-bold text-primaryBase">
               ວິໄສທັດ
             </h2>
             <div className="relative w-full h-56 mb-4">
               <Image src={Vision} alt="vision image" width={500} height={300} />
             </div>
-            <p className="text-center italic text-primary-foreground ">
+            <p className="italic text-center text-primary-foreground ">
               “ນະວັດຕະກໍາເທັກໂນໂລຍີທີ່ທັນສະໃໝ ພ້ອມແລ້ວທີ່ຈະສົ່ງມອບ ແລະ
               ບໍລິການໃຫ້ທ່ານ ດ້ວຍຄວາມເອົາໃຈໃສ່”
             </p>
           </div>
 
           {/* Right Column - Text Description */}
-          <div className="text-lg leading-relaxed text-gray-500 relative">
+          <div className="relative text-lg leading-relaxed text-gray-500">
             <p>
               ບໍລິສັດຄົງສົມບູນເຕັກໂນໂລຍີ (KHONGSOMBOUNTECHNOLOGY)
               ແມ່ນໄດ້ປ່ຽນຊື່ຈາກ ບໍລິສັດ ວຽງຈັນລະບົບຮັກສາຄວາມປອດໄພ ແລະ ໄອທີ
